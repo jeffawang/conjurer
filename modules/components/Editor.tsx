6050a26920dc9f93a3e73e733c5309ab8e0dd89c
Ben Rollin <ben.rollin@clever.com>
2023-04-07 02:19:09 -0700

    create pattern list sidebar

--- modules/components/Editor.tsx
+++ modules/components/Editor.tsx
@@ -7,11 +7,14 @@ export default function Editor() {
   return (
     <Box w="100vw" h="100vh">
       <Grid
-        templateAreas={`".        header"
+        templateAreas={`"patterns header"
                         "patterns display"
-                        "patterns timeline"`}
-        gridTemplateColumns="150px 1fr"
-        gap="1"
+                        "patterns timeline"
+                        "patterns ."`}
+        gridTemplateColumns="165px 1fr"
+        gridTemplateRows="auto auto auto 1fr"
+        gap={2}
+        height="100vh"
         fontWeight="bold"
       >
         <GridItem marginY="2" area="header">
@@ -19,7 +22,7 @@ export default function Editor() {
             <Heading>Conjurer</Heading>
           </VStack>
         </GridItem>
-        <GridItem pl="2" area="patterns">
+        <GridItem px="2" area="patterns" bgColor="gray.600">
           <PatternList />
         </GridItem>
         <GridItem pl="2" area="display">
--- modules/components/PatternList.tsx
+++ modules/components/PatternList.tsx
@@ -1,34 +1,43 @@
 import { patterns } from "@/modules/patterns/patterns";
-import { Box, Button, VStack } from "@chakra-ui/react";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 import { useMemo, useState } from "react";
 import { Block } from "../common/types/Block";
 import BlockView from "@/modules/components/BlockView";
 import { Canvas } from "@react-three/fiber";
 import { LED_COUNTS } from "@/modules/common/utils/size";
+import SelectablePattern from "@/modules/components/SelectablePattern";
+
+const PATTERN_PREVIEW_DISPLAY_FACTOR = 1.5;
 
 export default function PatternList() {
-  const [pattern, setPattern] = useState(patterns[0]);
-
+  const [selectedPatternIndex, setSelectedPatternIndex] = useState(0);
+  const pattern = patterns[selectedPatternIndex];
   const block = useMemo(() => new Block(pattern), [pattern]);
 
   return (
-    <VStack>
-      <Box width={`${LED_COUNTS.x}px`} height={`${LED_COUNTS.y}px`}>
+    <VStack mt={6}>
+      <Heading size="md">Pattern List</Heading>
+      <Text fontSize="xs">previewing</Text>
+      <Text lineHeight={0.5}>{pattern.name}</Text>
+      <Box
+        width={`${LED_COUNTS.x * PATTERN_PREVIEW_DISPLAY_FACTOR}px`}
+        height={`${LED_COUNTS.y * PATTERN_PREVIEW_DISPLAY_FACTOR}px`}
+      >
         <Canvas>
-          <BlockView key={block.pattern.name} autorun block={block} />
+          <BlockView key={pattern.name} autorun block={block} />
         </Canvas>
       </Box>
 
-      {patterns.map((p) => (
-        <Button
-          key={p.name}
-          colorScheme="teal"
-          variant="outline"
-          onClick={() => setPattern(p)}
-        >
-          {p.name}
-        </Button>
-      ))}
+      <VStack>
+        {patterns.map((p, i) => (
+          <SelectablePattern
+            key={p.name}
+            pattern={p}
+            selected={i === selectedPatternIndex}
+            onSelect={() => setSelectedPatternIndex(i)}
+          />
+        ))}
+      </VStack>
     </VStack>
   );
 }
