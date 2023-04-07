511f57bc1d2e242ca09b3755a9bd779341c5bee2
Ben Rollin <ben.rollin@clever.com>
2023-04-07 16:29:51 -0700

    allow resizing of blocks

--- modules/components/TimelineBlock.tsx
+++ modules/components/TimelineBlock.tsx
@@ -1,7 +1,8 @@
 import { Block } from "@/modules/common/types/Block";
 import { StandardParams } from "@/modules/common/types/PatternParams";
 import { timeToX, xToTime } from "@/modules/common/utils/time";
-import { Box, Card, Text, VStack } from "@chakra-ui/react";
+import TimelineBlockBound from "@/modules/components/TimelineBlockBound";
+import { Box, Card, HStack, Text, VStack } from "@chakra-ui/react";
 import { action } from "mobx";
 import { observer } from "mobx-react-lite";
 import { useRef, useState } from "react";
@@ -22,7 +23,18 @@ export default observer(function TimelineBlock({ block }: TimelineBlockProps) {
   const onDragStop = action(() => {
     block.startTime += xToTime(position.x);
     setPosition({ x: 0, y: 0 });
-    console.log(block.startTime);
+  });
+
+  const rightBoundDragNodeRef = useRef(null);
+  const [rightBoundDragging, setRightBoundDragging] = useState(false);
+  const [rightBoundPosition, setLeftBoundPosition] = useState({ x: 0, y: 0 });
+  const handleRightBoundDrag = (e: any, data: any) => {
+    setLeftBoundPosition({ x: data.x, y: 0 });
+  };
+  const onRightBoundDragStop = action(() => {
+    block.duration += xToTime(rightBoundPosition.x);
+    setLeftBoundPosition({ x: 0, y: 0 });
+    setRightBoundDragging(false);
   });
 
   return (
@@ -50,8 +62,26 @@ export default observer(function TimelineBlock({ block }: TimelineBlockProps) {
           <MdDragIndicator size={30} />
         </Box>
 
-        <VStack height="100%" justify="center">
-          <Text>{block.pattern.name}</Text>
+        <TimelineBlockBound
+          leftBound
+          onBoundChange={action((delta) => {
+            if (delta > block.duration) return;
+            block.startTime += delta;
+            block.duration -= delta;
+          })}
+        />
+        <TimelineBlockBound
+          rightBound
+          onBoundChange={action((delta) => {
+            if (block.duration + delta < 0) return;
+            block.duration += delta;
+          })}
+        />
+
+        <VStack pointerEvents="none" height="100%" justify="center">
+          <Text textOverflow="clip" overflowWrap="anywhere">
+            {block.pattern.name}
+          </Text>
         </VStack>
       </Card>
     </Draggable>
