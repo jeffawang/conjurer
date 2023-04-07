38163770040c4f5543835a2eb9d2d4c2d24f238e
Ben Rollin <ben.rollin@clever.com>
2023-04-07 02:50:15 -0700

    update starting blocks

--- modules/common/types/Store.ts
+++ modules/common/types/Store.ts
@@ -1,10 +1,9 @@
 import { Block } from "@/modules/common/types/Block";
 import { PatternParams } from "@/modules/common/types/PatternParams";
 import Timer from "@/modules/common/types/Timer";
-import GradientPattern from "@/modules/patterns/GradientPattern";
+import Rainbow from "@/modules/patterns/Rainbow";
 import SunCycle from "@/modules/patterns/SunCycle";
 import { makeAutoObservable, configure, runInAction } from "mobx";
-import { Vector2 } from "three";
 
 // Enforce MobX strict mode, which can make many noisy console warnings, but can help use learn MobX better.
 // Feel free to comment out the following if you want to silence the console messages.
@@ -38,21 +37,7 @@ export default class Store {
 
   initialize = () => {
     // Temporary hard-coded blocks
-    this.blocks.push(
-      new Block(SunCycle()),
-      new Block(
-        GradientPattern({
-          u_blah: {
-            name: "Blah",
-            value: 0,
-          },
-          u_a: {
-            name: "A",
-            value: new Vector2(),
-          },
-        }),
-      ),
-    );
+    this.blocks.push(new Block(SunCycle()), new Block(Rainbow()));
     this.blocks[0].setTiming(0, 7);
     this.blocks[1].setTiming(7, 3);
     this.initialized = true;
