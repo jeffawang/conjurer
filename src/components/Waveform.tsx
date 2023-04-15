8cad8dc49cf2051d0643a5337bf49df967f1d2ef
Ben Rollin <ben.rollin@clever.com>
2023-04-14 18:08:25 -0700

    convert to react3fiber

--- src/components/Waveform.tsx
+++ src/components/Waveform.tsx
@@ -1,89 +1,86 @@
 import { useStore } from "@/src/types/StoreContext";
 import { Box } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, memo, useState } from "react";
 import * as THREE from "three";
+import { IUniform } from "three";
 import waveformShaderSrc from "@/src/patterns/shaders/waveform.frag";
 import vert from "@/src/patterns/shaders/default.vert";
+import { Canvas } from "@react-three/fiber";
 
-export default observer(function Waveform() {
-  const initialized = useRef(false);
+export default memo(function Waveform() {
+  const [initialized, setInitialized] = useState(false);
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  const uniforms = useRef<Record<string, IUniform>>({});
+
   useEffect(() => {
-    if (initialized.current) return;
-
     const create = async () => {
-      const response = await fetch("/cloudkicker-explorebecurious.mp3");
-      const arrayBuffer = await response.arrayBuffer();
+      try {
+        const response = await fetch("/cloudkicker-explorebecurious.mp3");
+        const arrayBuffer = await response.arrayBuffer();
 
-      const audioContext = new AudioContext();
-      const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-      const audioData = audioBuffer.getChannelData(0);
-      const floatArray = Float32Array.from(audioData);
+        const audioContext = new AudioContext();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+        const audioData = audioBuffer.getChannelData(0);
+        const floatArray = Float32Array.from(audioData);
 
-      const sampleCount = audioBuffer.length;
-      const sampleTexWidth = 1024;
-      const sampleTexHeight = Math.floor(sampleCount / (4 * sampleTexWidth));
-      const sampleTex = new THREE.DataTexture(
-        floatArray,
-        sampleTexWidth,
-        sampleTexHeight,
-        THREE.RGBAFormat,
-        THREE.FloatType
-      );
-      sampleTex.needsUpdate = true;
+        const sampleCount = audioBuffer.length;
+        const sampleTexWidth = 1024;
+        const sampleTexHeight = Math.floor(sampleCount / (4 * sampleTexWidth));
+        const sampleTex = new THREE.DataTexture(
+          floatArray,
+          sampleTexWidth,
+          sampleTexHeight,
+          THREE.RGBAFormat,
+          THREE.FloatType
+        );
+        sampleTex.needsUpdate = true;
 
-      if (!canvasRef.current) return;
+        if (!canvasRef.current) return;
 
-      const renderer = new THREE.WebGLRenderer({
-        canvas: canvasRef.current,
-        alpha: false,
-      });
-      const uniforms = {
-        bgColor: { value: new THREE.Vector4(0.2, 0.3, 0.7, 1) },
-        activeColor: { value: new THREE.Vector4(0.9, 0.9, 0.9, 1) },
-        outWidth: { value: 1024.0 },
-        outHeight: { value: 40.0 },
-        sampleStart: { value: 0.0 },
-        sampleEnd: { value: audioData.length },
-        sampleWidth: { value: 1024.0 },
-        sampleHeight: { value: sampleTexHeight },
-        samples: { value: sampleTex },
-        numSamples: { value: audioData.length },
-      };
-      console.log(uniforms);
-      const geometry = new THREE.PlaneGeometry(2, 2);
-      const waveformShader = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: vert,
-        fragmentShader: waveformShaderSrc,
-      });
-      const mesh = new THREE.Mesh(geometry, waveformShader);
-      const scene = new THREE.Scene();
-      scene.add(mesh);
-      const camera = new THREE.OrthographicCamera(
-        -1, // Left
-        1, // Right
-        1, // Top
-        -1, // Bottom
-        0.1, // Near
-        10 // Far
-      );
-      camera.position.z = 1;
-      renderer.setSize(1024, 40); // Set the size of the renderer
-      renderer.render(scene, camera);
+        // const renderer = new THREE.WebGLRenderer({
+        //   canvas: canvasRef.current,
+        //   alpha: false,
+        // });
+        uniforms.current = {
+          bgColor: { value: new THREE.Vector4(0.2, 0.3, 0.7, 1) },
+          activeColor: { value: new THREE.Vector4(0.9, 0.9, 0.9, 1) },
+          outWidth: { value: 1024.0 },
+          outHeight: { value: 40.0 },
+          sampleStart: { value: 0.0 },
+          sampleEnd: { value: audioData.length },
+          sampleWidth: { value: 1024.0 },
+          sampleHeight: { value: sampleTexHeight },
+          samples: { value: sampleTex },
+          numSamples: { value: audioData.length },
+        };
+        setInitialized(true);
+      } catch (e) {
+        console.error(e);
+      }
     };
 
     create();
-    initialized.current = true;
 
     return () => {};
   }, []);
 
   return (
-    <Box position="absolute" top={1.5} width="100%">
-      <canvas id="itsamethecanvas" ref={canvasRef} />
+    <Box position="absolute" top={1.5} width="1024px" height="40px">
+      <Canvas dpr={1} ref={canvasRef}>
+        {initialized && (
+          <mesh>
+            <planeGeometry args={[2, 2]} />
+            <shaderMaterial
+              uniforms={uniforms.current}
+              fragmentShader={waveformShaderSrc}
+              vertexShader={vert}
+            />
+          </mesh>
+        )}
+      </Canvas>
     </Box>
   );
 });
--- src/patterns/shaders/waveform.frag
+++ src/patterns/shaders/waveform.frag
@@ -1,4 +1,3 @@
-
 uniform sampler2D samples;
 
 uniform vec4 bgColor;
@@ -15,22 +14,20 @@ uniform float numSamples;
 varying vec2 v_uv;
 
 float GetSample(float offset) {
-    float y = (offset/4.0) / sampleWidth;
-    float x = mod(offset/4.0, sampleWidth);
-    return abs(texture2D(samples, vec2(x,y)/vec2(sampleWidth,sampleHeight)))[int(mod(offset, 4.0))];
+    float y = (offset / 4.0) / sampleWidth;
+    float x = mod(offset / 4.0, sampleWidth);
+    return abs(texture2D(samples, vec2(x, y) / vec2(sampleWidth, sampleHeight)))[int(mod(offset, 4.0))];
 }
 
 void main() {
     float samplesPerPixel = ((sampleEnd - sampleStart) / outWidth) + 1.0;
     float start = sampleStart + (samplesPerPixel * gl_FragCoord.x);
     float sum = 0.0;
-    for (float i = start; i < start + samplesPerPixel; i++) {
+    for (float i = start; i < start + samplesPerPixel; i ++) {
         sum += GetSample(i);
     }
     float avg = sum / samplesPerPixel;
-    // float avg = 0.1;
     float yOffset = gl_FragCoord.y - (outHeight / 2.0);
     float barHeight = (avg * float(outHeight));
     gl_FragColor = abs(yOffset) < barHeight ? activeColor : bgColor;
-    // gl_FragColor = vec4(outHeight,outHeight,outHeight,1);
 }
