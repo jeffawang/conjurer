6670f20a2ae455ddd2acc8554169d24f2350df9b
Ben Rollin <ben.rollin@clever.com>
2023-04-24 00:04:55 -0700

    iterating towards effects on canopy

--- src/components/Canopy.tsx
+++ src/components/Canopy.tsx
@@ -1,10 +1,17 @@
-import { BufferAttribute, BufferGeometry, ShaderMaterial } from "three";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  ShaderMaterial,
+  WebGLRenderTarget,
+} from "three";
 import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/src/types/StoreContext";
-import vert from "@/src/patterns/shaders/canopy.vert";
+import vert from "@/src/patterns/shaders/default.vert";
+import canopyVert from "@/src/patterns/shaders/canopy.vert";
 import black from "@/src/patterns/shaders/black.frag";
+import fromTexture from "@/src/patterns/shaders/fromTexture.frag";
 import { LED_COUNTS, STRIP_LENGTH } from "@/src/utils/size";
 import catenary from "@/src/utils/catenary";
 
@@ -12,7 +19,16 @@ type CanopyViewProps = {};
 
 export default observer(function Canopy({}: CanopyViewProps) {
   const { currentBlock } = useStore();
+
+  const patternMesh = useRef<THREE.Mesh>(null);
+  // const effectMesh = useRef<THREE.Mesh>(null);
+  const canopyMesh = useRef<THREE.Points>(null);
+
+  const renderTarget = useMemo(() => new WebGLRenderTarget(512, 512), []);
+  // const effectUniforms = useRef({ u_texA: { value: renderTarget.texture } });
+
   const shaderMaterial = useRef<ShaderMaterial>(null);
+  const canopyUniforms = useRef({ u_tex: { value: renderTarget.texture } });
 
   const bufferGeometry = useMemo(() => {
     // TODO: fix this horrible hack
@@ -43,7 +59,7 @@ export default observer(function Canopy({}: CanopyViewProps) {
   }, []);
 
   const { timer } = useStore();
-  useFrame(() => {
+  useFrame(({ gl, camera }) => {
     // mobx linting will complain about these lines if observableRequiresReaction is enabled, but
     // it's fine. We don't want this function to react to changes in these variables - it runs every
     // frame already.
@@ -54,18 +70,48 @@ export default observer(function Canopy({}: CanopyViewProps) {
         globalTime - currentBlock.startTime,
         globalTime
       );
-  });
+
+    if (!patternMesh.current) return;
+
+    gl.setRenderTarget(renderTarget);
+    gl.render(patternMesh.current, camera);
+  }, 1);
+
+  useFrame(({ gl, camera }) => {
+    if (!canopyMesh.current) return;
+
+    gl.setRenderTarget(null);
+    gl.render(canopyMesh.current, camera);
+  }, 1);
 
   return (
-    <points>
-      <primitive attach="geometry" object={bufferGeometry} />
-      <shaderMaterial
-        key={currentBlock?.id}
-        ref={shaderMaterial}
-        uniforms={currentBlock?.pattern.params}
-        fragmentShader={currentBlock ? currentBlock.pattern.src : black}
-        vertexShader={vert}
-      />
-    </points>
+    <>
+      <mesh ref={patternMesh}>
+        <planeGeometry args={[2, 2]} />
+        <shaderMaterial
+          key={currentBlock?.id}
+          uniforms={currentBlock?.pattern.params}
+          fragmentShader={currentBlock ? currentBlock.pattern.src : black}
+          vertexShader={vert}
+        />
+      </mesh>
+      {/* <mesh ref={effectMesh}>
+        <planeGeometry args={[2, 2]} />
+        <shaderMaterial
+          uniforms={effectUniforms.current}
+          fragmentShader={redTint}
+          vertexShader={vert}
+        />
+      </mesh> */}
+      <points ref={canopyMesh}>
+        <primitive attach="geometry" object={bufferGeometry} />
+        <shaderMaterial
+          ref={shaderMaterial}
+          uniforms={canopyUniforms.current}
+          fragmentShader={fromTexture}
+          vertexShader={canopyVert}
+        />
+      </points>
+    </>
   );
 });
