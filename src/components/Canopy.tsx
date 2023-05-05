a8de6b8ce1a887f1603e71b92b633778814a8dd7
Ben Rollin <ben.rollin@clever.com>
2023-05-04 22:32:14 -0700

    fixed canopy vertex position calculation hack

--- src/components/Canopy.tsx
+++ src/components/Canopy.tsx
@@ -3,7 +3,13 @@ import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
 import canopyVert from "@/src/patterns/shaders/canopy.vert";
 import fromTexture from "@/src/patterns/shaders/fromTexture.frag";
-import { LED_COUNTS, STRIP_LENGTH } from "@/src/utils/size";
+import {
+  APEX_HEIGHT,
+  APEX_RADIUS,
+  BASE_RADIUS,
+  LED_COUNTS,
+  STRIP_LENGTH,
+} from "@/src/utils/size";
 import catenary from "@/src/utils/catenary";
 
 type CanopyViewProps = {
@@ -15,30 +21,35 @@ export default function Canopy({ renderTarget }: CanopyViewProps) {
   const canopyUniforms = useRef({ u_texture: { value: renderTarget.texture } });
 
   const bufferGeometry = useMemo(() => {
-    // TODO: fix this horrible hack
     const catenaryCoordinates = catenary(
-      { x: 1, y: 0 },
-      { x: 8, y: 0 },
+      { x: APEX_RADIUS, y: APEX_HEIGHT },
+      { x: BASE_RADIUS, y: 0 },
       STRIP_LENGTH,
       LED_COUNTS.y
     );
     const ledPositions = [];
+    const uv = [];
     for (let x = 0; x < LED_COUNTS.x; x++) {
       for (let y = 0; y < LED_COUNTS.y; y++) {
+        const normalizedX = x / (LED_COUNTS.x - 1);
+        const normalizedY = y / (LED_COUNTS.y - 1);
+        const theta = normalizedX * 2 * Math.PI;
+
+        uv.push(normalizedX, normalizedY);
         ledPositions.push(
-          x / (LED_COUNTS.x - 1),
-          y / (LED_COUNTS.y - 1),
+          catenaryCoordinates[y][0] * Math.cos(theta),
+          catenaryCoordinates[y][0] * Math.sin(theta),
           -catenaryCoordinates[y][1]
         );
       }
     }
-    const positionsFloatArray = new Float32Array(ledPositions);
 
     const geometry = new BufferGeometry();
     geometry.setAttribute(
       "position",
-      new BufferAttribute(positionsFloatArray, 3)
+      new BufferAttribute(new Float32Array(ledPositions), 3)
     );
+    geometry.setAttribute("uv", new BufferAttribute(new Float32Array(uv), 2));
     return geometry;
   }, []);
 
--- src/patterns/shaders/canopy.vert
+++ src/patterns/shaders/canopy.vert
@@ -4,21 +4,12 @@ varying vec2 v_uv;
 
 void main() {
     gl_PointSize = 4.0;
-    float apexRadius = 1.0; // ft
-    float canopyRadius = 8.0; // ft
-
-    float radius = apexRadius + (canopyRadius - apexRadius) * position.y;
-    float theta = position.x * 2.0 * PI;
-
-    float x = radius * cos(theta);
-    float y = radius * sin(theta);
 
     // Pass the uv coordinates to the fragment shader.
-    // x, y are in the range [0, 1]
+    // uv.x, uv.y are in the range [0, 1]
     // x represents the angle around the center of the canopy
     // y represents the distance from the apex of the canopy
-    v_uv = position.xy;
+    v_uv = uv;
 
-    // TODO: current hack, z is being piped through directly
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(x, y, position.z, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
