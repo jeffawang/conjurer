04e93cb2fcdbc1cb40cd7ccce93bc6cef53bcc9c
Ben Rollin <ben.rollin@clever.com>
2023-05-06 03:46:10 -0700

    make preview of canopy

---
+++ src/components/CameraControls.tsx
@@ -0,0 +1,17 @@
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { PerspectiveCamera as PerspectiveCameraThree, Vector3 } from "three";
+import { memo, useRef } from "react";
+
+export default memo(function CameraControls() {
+  const cameraRef = useRef<PerspectiveCameraThree>();
+  return (
+    <>
+      <PerspectiveCamera
+        ref={cameraRef}
+        makeDefault
+        position={new Vector3(0, 0, 20)}
+      />
+      <OrbitControls camera={cameraRef.current} />
+    </>
+  );
+});
--- src/components/DisplayCanvas.tsx
+++ src/components/DisplayCanvas.tsx
@@ -1,40 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import Canopy from "@/src/components/Canopy";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { PerspectiveCamera as PerspectiveCameraThree, Vector3 } from "three";
-import { useRef } from "react";
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { Perf } from "r3f-perf";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/src/types/StoreContext";
 import RenderPipeline from "@/src/components/RenderPipeline";
 import CartesianView from "@/src/components/CartesianView";
+import ThreeEffects from "@/src/components/ThreeEffects";
+import CameraControls from "@/src/components/CameraControls";
 
 export default observer(function DisplayCanvas() {
   const { uiStore } = useStore();
 
-  const cameraRef = useRef<PerspectiveCameraThree>();
   return (
     <Canvas>
-      <PerspectiveCamera
-        ref={cameraRef}
-        makeDefault
-        position={new Vector3(0, 0, 20)}
-      />
-      <OrbitControls camera={cameraRef.current} />
       {uiStore.showingPerformance && <Perf />}
-
-      {uiStore.displayingCanopy && (
-        <EffectComposer>
-          {/* EffectComposer upon initializing causes the warning: WebGL context was lost. */}
-          <Bloom
-            // TODO: play with these values more
-            intensity={0.1}
-            luminanceThreshold={0.01}
-          />
-        </EffectComposer>
-      )}
-
+      <CameraControls />
       <RenderPipeline>
         {(renderTarget) =>
           uiStore.displayingCanopy ? (
@@ -44,6 +24,7 @@ export default observer(function DisplayCanvas() {
           )
         }
       </RenderPipeline>
+      {uiStore.displayingCanopy && <ThreeEffects />}
     </Canvas>
   );
 });
