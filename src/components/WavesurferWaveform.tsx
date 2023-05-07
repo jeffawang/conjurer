ab070ca66cf68d62d333eed1958c14a99b4a28e9
Ben Rollin <ben.rollin@clever.com>
2023-05-07 15:29:26 -0700

    wave surfer clean up

--- src/components/WavesurferWaveform.tsx
+++ src/components/WavesurferWaveform.tsx
@@ -5,24 +5,27 @@ import { useRef, useEffect } from "react";
 import { clamp } from "three/src/math/MathUtils";
 import { useDebouncedCallback } from "use-debounce";
 import type WaveSurfer from "wavesurfer.js";
-import type { GenericPlugin } from "wavesurfer.js/dist/base-plugin";
+import type { WaveSurferOptions } from "wavesurfer.js";
 import type TimelinePlugin from "wavesurfer.js/dist/plugins/timeline";
+import type { TimelinePluginOptions } from "wavesurfer.js/dist/plugins/timeline";
+import type { GenericPlugin } from "wavesurfer.js/dist/base-plugin";
 import {
   AUDIO_BUCKET_NAME,
   AUDIO_BUCKET_PREFIX,
   AUDIO_BUCKET_REGION,
 } from "@/src/utils/audio";
 
-const TIMELINE_OPTIONS = {
+const TIMELINE_OPTIONS: TimelinePluginOptions = {
   height: 40,
   insertPosition: "beforebegin" as const,
   timeInterval: 0.25,
   primaryLabelInterval: 5,
   secondaryLabelInterval: 0,
   style: {
-    fontSize: "12px",
+    fontSize: "14px",
     color: "#000000",
     zIndex: 10,
+    // webkitTextStroke: "1px #000000",
   } as any,
 };
 
@@ -57,15 +60,12 @@ export const WavesurferWaveform = observer(function WavesurferWaveform() {
       const timeline = TimelinePlugin.create(TIMELINE_OPTIONS);
 
       // https://wavesurfer-js.org/docs/options.html
-      const options = {
+      const options: WaveSurferOptions = {
         container: waveformRef.current!,
         waveColor: "#ddd",
         progressColor: "#0178FF",
         cursorColor: "#00000000",
-        responsive: true,
         height: 40,
-        normalize: true,
-        partialRender: true,
         hideScrollbar: true,
         fillParent: false,
         interact: false,
