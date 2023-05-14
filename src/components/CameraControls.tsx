07709e98d3884668e10d8fbd6e4541efb80bddc1
Ben Rollin <ben.rollin@clever.com>
2023-05-14 16:54:43 -0700

    adjust preview camera position

--- src/components/CameraControls.tsx
+++ src/components/CameraControls.tsx
@@ -2,14 +2,20 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { PerspectiveCamera as PerspectiveCameraThree, Vector3 } from "three";
 import { memo, useRef } from "react";
 
-export const CameraControls = memo(function CameraControls() {
+type CameraControlsProps = {
+  initialPosition?: Vector3;
+};
+
+export const CameraControls = memo(function CameraControls({
+  initialPosition = new Vector3(0, 0, 20),
+}: CameraControlsProps) {
   const cameraRef = useRef<PerspectiveCameraThree>();
   return (
     <>
       <PerspectiveCamera
         ref={cameraRef}
         makeDefault
-        position={new Vector3(0, 0, 20)}
+        position={initialPosition}
       />
       <OrbitControls camera={cameraRef.current} />
     </>
