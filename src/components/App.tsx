f6ac081e6b92cbe0a7572d020ca7d29028cf136c
Ben Rollin <ben.rollin@clever.com>
2023-05-14 01:38:29 -0700

    horizontal and vertical layout options

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,11 @@ import { Display } from "@/src/components/Display";
 import { PatternList } from "@/src/components/PatternList";
 import { useEffect, useRef } from "react";
 import { useStore } from "@/src/types/StoreContext";
+import { observer } from "mobx-react-lite";
 
-export const App = function App() {
+export const App = observer(function App() {
   const store = useStore();
+  const { uiStore } = store;
   const didInitialize = useRef(false);
   useEffect(() => {
     if (didInitialize.current) return;
@@ -14,27 +16,42 @@ export const App = function App() {
     store.initialize();
   }, [store]);
 
+  const gridItems = (
+    <>
+      <GridItem px="2" area="patterns" bgColor="gray.600">
+        <PatternList />
+      </GridItem>
+      <GridItem area="display">
+        <Display />
+      </GridItem>
+      <GridItem area="arrangement">
+        <Arrangement />
+      </GridItem>
+    </>
+  );
+
   return (
     <Box w="100vw" h="100vh">
-      <Grid
-        templateAreas={`"patterns display"
+      {uiStore.horizontalLayout ? (
+        <Grid
+          templateAreas={`"patterns display"
                         "patterns arrangement"`}
-        gridTemplateColumns="165px calc(100vw - 165px)"
-        gridTemplateRows="min-content 1fr"
-        height="100vh"
-        fontWeight="bold"
-      >
-        <GridItem area="header"></GridItem>
-        <GridItem px="2" area="patterns" bgColor="gray.600">
-          <PatternList />
-        </GridItem>
-        <GridItem area="display">
-          <Display />
-        </GridItem>
-        <GridItem area="arrangement">
-          <Arrangement />
-        </GridItem>
-      </Grid>
+          gridTemplateColumns="165px calc(100vw - 165px)"
+          gridTemplateRows="min-content 1fr"
+          height="100vh"
+        >
+          {gridItems}
+        </Grid>
+      ) : (
+        <Grid
+          templateAreas={`"patterns arrangement display"`}
+          gridTemplateColumns="165px calc(50vw - 165px) calc(50vw)"
+          gridTemplateRows="100vh"
+          height="100vh"
+        >
+          {gridItems}
+        </Grid>
+      )}
     </Box>
   );
-};
+});
--- src/components/DisplayControls.tsx
+++ src/components/DisplayControls.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react-lite";
 import { useStore } from "@/src/types/StoreContext";
 import { action } from "mobx";
 import { FaDotCircle } from "react-icons/fa";
+import { TbLayoutRows, TbLayoutColumns } from "react-icons/tb";
 import { TbRectangleFilled } from "react-icons/tb";
 import { AiOutlineLineChart } from "react-icons/ai";
 import { UserPicker } from "@/src/components/UserPicker";
@@ -12,6 +13,19 @@ export const DisplayControls = observer(function DisplayControls() {
   return (
     <>
       <VStack p={2} position="absolute" top={0} left={0} zIndex={1}>
+        <IconButton
+          aria-label="Toggle horizontal/vertical layout"
+          title="Toggle horizontal/vertical layout"
+          height={6}
+          icon={
+            uiStore.horizontalLayout ? (
+              <TbLayoutColumns size={17} />
+            ) : (
+              <TbLayoutRows size={17} />
+            )
+          }
+          onClick={action(() => uiStore.toggleLayout())}
+        />
         <IconButton
           aria-label="Toggle canopy view"
           title="Toggle canopy view"
